0f34ab8009ce2a2ac6774e41f9732856e47cfc9c
Daniel Facciolo Pires <daniel@unifacef.edu.br>
2025-03-17 22:12:02 -0300

    Carro.java

--- Projeto/Lista2/src/Main.java
+++ Projeto/Lista2/src/Main.java
@@ -1,3 +1,5 @@
+import exe0.Carro;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -5,5 +7,23 @@ public class Main {
         Carro obj1 = new Carro();
       //  obj1.marca = "Fiat";
         obj1.setMarca("Fiat");
+      //  obj1.modelo = "Palio";
+        obj1.setModelo("Palio");
+      //  obj1.ano = 2020;
+        obj1.setAno(2020);
+      //  obj1.velocidade = 0;
+        obj1.setVelocidade(0);
+      //  System.out.println("Modelo " + obj1.modelo);
+        System.out.println("Modelo " + obj1.getModelo() +
+                " Marca " + obj1.getMarca());
+
+        Carro obj2 = new Carro("GM", "Onix", -2019, 60);
+        System.out.println("Ano " + obj2.getAno() +
+                " Vel " + obj2.getVelocidade());
+        obj2.acelerar(80);
+        obj2.frear(30);
+        System.out.println(obj2.exibeDetalhes());
+        //obj2.limita100();
+
     }
 }
